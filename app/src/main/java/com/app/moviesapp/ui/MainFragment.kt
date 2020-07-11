f7d01749bad425c85eb4a6b87ef0f11d0d85b35b
damrek <dmarecas@gmail.com>
2020-07-11 11:34:00 +0200

    Navigation components config

---
+++ app/src/main/java/com/app/moviesapp/ui/MainFragment.kt
@@ -0,0 +1,33 @@
+package com.app.moviesapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.app.moviesapp.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_movie_detail.setOnClickListener {
+            findNavController().navigate(R.id.movieDetailFragment)
+        }
+    }
+}
